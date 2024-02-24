82cf2240284daa3fd5981551629f93203787f4ce
Vijay Pandey <vijaypandey@BFSL.COM>
2024-02-24 22:11:46 +0530

    changes the code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", authRouter);
 //Port and Connect to DB
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
